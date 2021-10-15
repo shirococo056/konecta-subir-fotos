b5eef8fe0e31c89daa879f7c02a9a151a2e4372d
shirococo056 <91107435+shirococo056@users.noreply.github.com>
2021-10-15 19:57:21 -0300

    asfas

--- src/components/sidebar/suggested-profile.js
+++ src/components/sidebar/suggested-profile.js
@@ -1,50 +1,20 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import { updateLoggedInUserFollowing, updateFollowedUserFollowers } from '../../services/firebase';
 
-export default function SuggestedProfile({
-  profileDocId,
-  username,
-  profileId,
-  userId,
-  loggedInUserDocId
-}) {
-  const [followed, setFollowed] = useState(false);
+export default function SuggestedProfile({ username }) {
+  const [followed] = useState(false);
 
-  async function handleFollowUser() {
-    setFollowed(true);
-    await updateLoggedInUserFollowing(loggedInUserDocId, profileId, false);
-    await updateFollowedUserFollowers(profileDocId, userId, false);
-  }
+  //  async function handleFollowUser() {setFollowed(true);await updateLoggedInUserFollowing(loggedInUserDocId, profileId, false);await updateFollowedUserFollowers(profileDocId, userId, false);}
 
   return !followed ? (
     <div className="flex flex-row items-center align-items justify-between">
       <div className="flex items-center justify-between">
-        <img
-          className="rounded-full w-8 flex mr-3"
-          src={`/images/avatars/${username}.jpg`}
-          alt=""
-        />
-        <Link to={`/p/${username}`}>
-          <p className="font-bold text-sm">{username}</p>
-        </Link>
+        <p className="font-bold text-sm">{username}</p>
       </div>
-      <button
-        className="text-xs font-bold text-blue-medium"
-        type="button"
-        onClick={handleFollowUser}
-      >
-        Follow
-      </button>
     </div>
   ) : null;
 }
 
 SuggestedProfile.propTypes = {
-  profileDocId: PropTypes.string.isRequired,
-  username: PropTypes.string.isRequired,
-  profileId: PropTypes.string.isRequired,
-  userId: PropTypes.string.isRequired,
-  loggedInUserDocId: PropTypes.string.isRequired
+  username: PropTypes.string.isRequired
 };
