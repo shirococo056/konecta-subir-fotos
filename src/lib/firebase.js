168d2590f35e295ada0b37dbb890f8f85679f07a
shirococo056 <91107435+shirococo056@users.noreply.github.com>
2021-10-15 19:40:03 -0300

    ASFDASD

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,17 +4,14 @@ import 'firebase/auth';
 
 // replace these with your own config values from Firebase!
 const config = {
-  apiKey: 'AIzaSyAK2oMYTzZGUiV47YARTl6uPkyzjoWqsN0',
-
-  authDomain: 'konecta-pruebas.firebaseapp.com',
-
-  projectId: 'konecta-pruebas',
-
-  storageBucket: 'konecta-pruebas.appspot.com',
-
-  messagingSenderId: '714228604689',
-
-  appId: '1:714228604689:web:3c70a8709f7b33d2c136b0'
+  apiKey: 'AIzaSyCFHuM-dhUN45L2yCWE-Z8L6aWryPk098A',
+  authDomain: 'konecta-instagram.firebaseapp.com',
+  databaseURL: 'https://konecta-instagram-default-rtdb.firebaseio.com',
+  projectId: 'konecta-instagram',
+  storageBucket: 'konecta-instagram.appspot.com',
+  messagingSenderId: '670817656287',
+  measurementId: 'G-HC0D29WZKG',
+  appId: '1:670817656287:web:a9bee5a4251033a7a9f86a'
 };
 
 const firebase = Firebase.initializeApp(config);
