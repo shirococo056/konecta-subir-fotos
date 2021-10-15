9b8e202f03644ad644192ade77e2ccc84e6e4b2b
shirococo056 <91107435+shirococo056@users.noreply.github.com>
2021-10-15 19:42:14 -0300

    asdasd

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -6,7 +6,7 @@ export default function Sidebar() {
   const { user: { docId = '', userId, following } = {} } = useContext(LoggedInUserContext);
 
   return (
-    <div className="rounded flex flex-col">
+    <div className="col-span-3 grid grid-cols-3 lg:grid-cols-3">
       <Suggestions userId={userId} following={following} loggedInUserDocId={docId} />
     </div>
   );
