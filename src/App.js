6d2a3f704fc2beb64be6b62505a22c1b469bed97
shirococo056 <91107435+shirococo056@users.noreply.github.com>
2021-10-16 20:32:20 -0300

    sadf

--- src/App.js
+++ src/App.js
@@ -1,35 +1,112 @@
-import { lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import * as ROUTES from './constants/routes';
-import UserContext from './context/user';
-import useAuthListener from './hooks/use-auth-listener';
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
 
-import ProtectedRoute from './helpers/protected-route';
+import { View, Text } from 'react-native';
 
-const Login = lazy(() => import('./pages/login'));
-const SignUp = lazy(() => import('./pages/sign-up'));
-const Dashboard = lazy(() => import('./pages/dashboard'));
-const Profile = lazy(() => import('./pages/profile'));
-const NotFound = lazy(() => import('./pages/not-found'));
+import * as firebase from 'firebase';
 
-export default function App() {
-  const { user } = useAuthListener();
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
-  return (
-    <UserContext.Provider value={{ user }}>
-      <Router>
-        <Suspense fallback={<p>Loading...</p>}>
-          <Switch>
-            <Route path={ROUTES.LOGIN} component={Login} />
-            <Route path={ROUTES.SIGN_UP} component={SignUp} />
-            <Route path={ROUTES.PROFILE} component={Profile} />
-            <ProtectedRoute user={user} path={ROUTES.DASHBOARD} exact>
-              <Dashboard />
-            </ProtectedRoute>
-            <Route component={NotFound} />
-          </Switch>
-        </Suspense>
-      </Router>
-    </UserContext.Provider>
-  );
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import rootReducer from './redux/reducers';
+
+import LandingScreen from './components/auth/Landing';
+import RegisterScreen from './components/auth/Register';
+import LoginScreen from './components/auth/Login';
+import MainScreen from './components/Main';
+import AddScreen from './components/main/Add';
+import SaveScreen from './components/main/Save';
+import CommentScreen from './components/main/Comment';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAGV2HfQSQtFCjD9xbu4RDUiTewa7is8O8',
+  authDomain: 'instagram-dev-8b9c4.firebaseapp.com',
+  databaseURL: 'https://instagram-dev-8b9c4.firebaseio.com',
+  projectId: 'instagram-dev-8b9c4',
+  storageBucket: 'instagram-dev-8b9c4.appspot.com',
+  messagingSenderId: '386631361638',
+  appId: '1:386631361638:web:d25aa4b5ec7911c57679a7',
+  measurementId: 'G-GQYEJ3XWBV'
+};
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
 }
+
+const Stack = createStackNavigator();
+
+export class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      loaded: false
+    };
+  }
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (!user) {
+        this.setState({
+          loggedIn: false,
+          loaded: true
+        });
+      } else {
+        this.setState({
+          loggedIn: true,
+          loaded: true
+        });
+      }
+    });
+  }
+
+  render() {
+    const { loggedIn, loaded } = this.state;
+    if (!loaded) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <Text>Loading</Text>
+        </View>
+      );
+    }
+
+    if (!loggedIn) {
+      return (
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Landing">
+            <Stack.Screen
+              name="Landing"
+              component={LandingScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen name="Register" component={RegisterScreen} />
+            <Stack.Screen name="Login" component={LoginScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      );
+    }
+
+    return (
+      <Provider store={store}>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Main">
+            <Stack.Screen name="Main" component={MainScreen} />
+            <Stack.Screen name="Add" component={AddScreen} navigation={this.props.navigation} />
+            <Stack.Screen name="Save" component={SaveScreen} navigation={this.props.navigation} />
+            <Stack.Screen
+              name="Comment"
+              component={CommentScreen}
+              navigation={this.props.navigation}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Provider>
+    );
+  }
+}
+
+export default App;
